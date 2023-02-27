5a7bac63d7c571a2b8055455a2b8955fdc5f3e4c
Ethan Fang <ethan.fang@gmail.com>
2023-02-27 18:03:57 +1100

    First commit of React Timer

---
+++ app/components/Main.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+var Navigation = require("Navigation")
+
+var Main = (props) => {
+    return (
+        <div>
+            <Navigation></Navigation>
+    </div>
+    );
+}
+
+module.exports = Main;
