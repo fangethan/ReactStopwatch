0c8c82828c44fc11299ac42ea40b8366b88d6cc1
Ethan Fang <ethan.fang@gmail.com>
2023-03-02 12:03:33 +1100

    Created countdown clock

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,15 @@
 var React = require("react");
+var PropTypes = require('prop-types');
 
 var Clock = React.createClass({
+    getDefaultProps: function () {
+        totalSeconds: 0
+    },
+
+    propTypes: {
+        totalSeconds: PropTypes.number,
+    },
+
     formatSeconds: function (totalSeconds){
         var seconds = totalSeconds % 60;
         var minutes = Math.floor(totalSeconds / 60);
@@ -17,9 +26,13 @@ var Clock = React.createClass({
     },
 
     render() {
-        return (
-            <div>
-                <h1>Clock</h1>
+        var {totalSeconds} = this.props;
+
+        return (            
+            <div className="clock">
+                <span className="clock-text">
+                    {this.formatSeconds(totalSeconds)}
+                </span>
             </div>
         )
     }
--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -10,7 +10,16 @@ var Clock = require("Clock");
 describe("Clock", () => {
     it("should exist", () => {
         expect(Clock).toExist();
-    })
+    });
+
+    describe("render", () => {
+        it("should render clock to output", () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62} />)
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find(".clock-text").text();
+            expect(actualText).toBe("01:02")
+        })
+    });
 
     describe("formatSeconds", () => {
         it("should format seconds", () => {
