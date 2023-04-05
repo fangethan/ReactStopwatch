ed0ddf989353651c8cb1367a5ba96d016c16fc7e
Ethan Fang <ethan.fang@gmail.com>
2023-04-05 11:35:23 +1000

    Add componentWillUnmount and update status when timer hits 0

--- public/bundle.js
+++ public/bundle.js
@@ -26281,6 +26281,11 @@
 	        }
 	    },
 
+	    componentWillUnmount: function componentWillUnmount() {
+	        clearInterval(this.timer);
+	        this.timer = undefined;
+	    },
+
 	    startTimer: function startTimer() {
 	        var _this = this;
 
@@ -26289,6 +26294,9 @@
 	            _this.setState({
 	                count: newCount >= 0 ? newCount : 0
 	            });
+	            if (newCount == 0) {
+	                _this.setState({ countdownStatus: "stopped" });
+	            }
 	        }, 1000);
 	    },
 
