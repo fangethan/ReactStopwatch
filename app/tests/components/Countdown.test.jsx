112240a24fb2a726839fbf675bf6c7eccbd1ff5f
Ethan Fang <ethan.fang@gmail.com>
2023-04-05 11:55:37 +1000

    Add test cases for pause and stop

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -24,15 +24,40 @@ describe("Countdown", () => {
                 done();
             }, 1001)
         });
-        it("should set state to be sarted and countdown and not go negative in timer", () => {
+        it("should set state to be started and countdown and not go negative in timer", () => {
             var countdown = TestUtils.renderIntoDocument(<Countdown />);
             countdown.handleSetCountdown(3);
-            
+
             setTimeout(() => {
                 expect(countdown.state.count).toBe(0);
                 done();
             }, 3001)
         });
+
+        it("should pause countdown on paused status", () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown />)
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange("paused");
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe("paused");
+                done();
+            }, 1001)
+        });
+
+        it("should reset countdown on stopped status", () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown />)
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange("stopped");
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe("stopped");
+                done();
+            }, 1001)
+        });
+
     });
 
 });
